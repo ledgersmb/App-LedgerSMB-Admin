a82ab3b4a698c4aafded66749a3a13695088e477
Chris Travers <chris@efficito.com>
2014-09-13 06:14:36 -0700

    new files

---
+++ lib/App/LedgerSMB/Admin.pm
@@ -0,0 +1,132 @@
+package App::LedgerSMB::Admin;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+App::LedgerSMB::Admin - Easily Manage LedgerSMB Installations
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+This module provides the basic routines for managing multiple major versions of
+LedgerSMB through a consistent toolkit.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 add_paths(%versionhash)
+
+=cut
+
+my %version_paths;
+
+sub add_paths{
+    shift if $_[0] eq __PACKAGE__;
+    my %version_hash = @_;
+    %version_paths = (%version_paths, %version_hash);
+}
+
+=head2 path_for($major_version)
+
+=cut
+
+sub path_for{
+    shift if $_[0] eq __PACKAGE__;
+    return $version_paths{$_[0]};
+}
+
+=head1 AUTHOR
+
+Chris Travers, C<< <chris at efficito.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-ledgersmb-admin at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-LedgerSMB-Admin>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::LedgerSMB::Admin
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-LedgerSMB-Admin>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-LedgerSMB-Admin>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-LedgerSMB-Admin>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-LedgerSMB-Admin/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Chris Travers.
+
+This program is distributed under the (Revised) BSD License:
+L<http://www.opensource.org/licenses/BSD-3-Clause>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of Chris Travers's Organization
+nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written
+permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+1; # End of App::LedgerSMB::Admin
---
+++ lib/App/LedgerSMB/Admin/Database.pm
@@ -0,0 +1,211 @@
+package App::LedgerSMB::Admin::Database;
+use Moo;
+extends 'PGObject::Util::DBAdmin';
+use App::LedgerSMB::Admin;
+use App::LedgerSMB::Admin::Database::Setting;
+
+=head1 NAME
+
+App::LedgerSMB::Admin::Database - Administer LedgerSMB Databases
+
+=head1 SYNOPSIS
+
+Upgrading to 1.4 from 1.3, after updating 1.3 instance to latest:
+
+  use App::LedgerSMB::Admin;
+  use App::LedgerSMB::Admin::Database;
+  App::LedgerSMB::Admin->add_paths(
+     '1.3' => '/usr/local/ledgersmb_1.3',
+     '1.4' => '/usr/local/ledgersmb_1.4',
+  ); # setting the version paths
+  my $db = App::LedgerSMB::Admin::Database->new(
+     username  => 'postgres',
+     password  => 'secretpassword',
+     host      => 'localhost',
+     port      => '5432',
+     dbname    => 'mycompany',
+  );
+  if ($db->major_version eq '1.3') {
+      $db->reload;
+      $db->upgrade_to('1.4');
+  }
+
+=head1 VERSION
+
+0.01
+
+=cut
+
+our $VERSION=0.01;
+
+=head1 PROPERTIES INHERITED FROM PGObject::Util::DBAdmin
+
+Please see the docs for PGObject::Util::DBAdmin.
+
+=head2 username 
+
+=head2 password
+
+=head2 host
+
+=head2 port
+
+=head2 dbname
+
+=head1 ADDITIONAL PROPERTIES
+
+=head2 version
+
+Returns the version number of the database.
+
+=cut
+
+has version => (is => 'lazy');
+
+sub _build_version {
+    my $self = shift;
+    return App::LedgerSMB::Admin::Database::Setting->get(
+                                        database    => $self,
+                                        setting_key => 'version')->value;
+}
+
+=head2 major_version
+
+Major versions are generally understood to be not backwards compatible.  In
+LedgerSMB, as with PostgreSQL, major versions are based on the second numbers
+in the version, so 1.2, 1.3, and 1.4 are major versions.
+
+=cut
+
+has major_version => (is => 'lazy');
+
+sub _build_major_version {
+    my $self = shift;
+    my $version = $self->version;
+    $version =~ s/\.\d*$//;
+    return $version;
+}
+
+=head1 METHODS INHERITED
+
+Please see the docs for PGObject::Util::DBAdmin
+
+=head2 create
+
+=head2 connect
+
+=head2 drop
+
+=head2 run_file
+
+=head2 backup
+
+=head2 restore_backup
+
+=head1 NEW METHODS
+
+=head2 load($major_version)
+
+Loads the db schema for the major version requested.
+
+=cut
+
+sub load {
+    my ($self, $major_version) = @_;
+    $self->run_file(
+            file => App::LedgerSMB::Admin->path_for($major_version)
+                    . "/sql/Pg-Database.sql"
+    );
+    return $self->reload;
+}
+
+=head2 reload
+
+Reloads all modules in a LedgerSMB instance.
+
+=cut
+
+sub reload {
+    my ($self) = @_;
+    my $sqlpath = App::LedgerSMB::Admin->path_for($self->major_version)
+                  . '/sql/modules';
+    return $self->process_loadorder($sqlpath, "$sqlpath/LOADORDER");
+}
+
+=head2 process_loadorder($sql_path, $loadorder_path);
+
+Processes a specific loadorder.  Useful for installing extensions to LedgerSMB
+
+Dies if any SQL files produce errors except from a file starting with "Fixes."
+
+=cut
+
+sub process_loadorder {
+    my ($self, $sql_path, $loadorder_path) = @_;
+    $sql_path =~ s|/$||;
+    open(LOAD, '<', $loadorder_path);
+    for my $line (<LOAD>){
+        $line =~ s/(\s*|#.*)//g;
+        next unless $line;
+        if ($line =~ /^Fixes/){
+           eval { $self->run_file(file => "$sql_path/$line") };
+        } else {
+           $self->run_file(file => "$sql_path/$line");
+        }
+    }
+}
+
+=head2 upgrade_to($major_version)
+
+Upgrades to the major version specified.  Provides an error if the upgrade file
+is not found.
+
+=cut
+
+sub upgrade_to {
+    my ($self, $major_version) = @_;
+    my $up_filename = "lsmb" . $self->major_version . '-' . $major_version
+                      . ".sql";
+    my $versionpath = App::LedgerSMB::Admin->path_for($major_version);
+    die 'No version path registered' unless $versionpath;
+    $self->run_file(file => "$versionpath/$up_filename");
+}
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Chris Travers.
+
+This program is distributed under the (Revised) BSD License:
+L<http://www.opensource.org/licenses/BSD-3-Clause>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+* Neither the name of Chris Travers's Organization
+nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written
+permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=cut
+
+1;	;
