b234d4e096e0337462828f8d22dcdc3e24412a56
Chris Travers <chris@efficito.com>
2014-09-25 08:40:01 -0700

    Some refinements to url mappings.

--- lib/App/LedgerSMB/Admin/Database/Routings.pm
+++ lib/App/LedgerSMB/Admin/Database/Routings.pm
@@ -3,22 +3,22 @@ use Template;
 
 # Databases
 
-get  '/lsmbadmin/1.0/:host/:port/' => \&list_dbs;
-get  '/lsmbadmin/1.0/:host/:port/newdb/' => sub { template 'new_db' => {} };
-post '/lsmbadmin/1.0/:host/:port/newdb/' => \&createdb;
-put  '/lsmbadmin/1.0/:host/:port/newdb/' => \&createdb;
-get  '/lsmbadmin/1.0/:host/:port/globals/' => \&backup_globals;
-post '/lsmbadmin/1.0/:host/:port/globals/' => \&restore_globals;
-put  '/lsmbadmin/1.0/:host/:port/globals/' => \&restore_globals;
-get  '/lsmbadmin/1.0/:host/:port/db/:dbname' => sub {
+get  '/lsmbadmin/1.0/:host/:port/list.*' => \&list_dbs;
+get  '/lsmbadmin/1.0/:host/:port/new.html' => sub { template 'new_db' => {} };
+post '/lsmbadmin/1.0/:host/:port/new.*' => \&createdb;
+put  '/lsmbadmin/1.0/:host/:port/new.*' => \&createdb;
+get  '/lsmbadmin/1.0/:host/:port/globals/*.*' => \&backup_globals;
+post '/lsmbadmin/1.0/:host/:port/globals/*.*' => \&restore_globals;
+put  '/lsmbadmin/1.0/:host/:port/globals/*.*' => \&restore_globals;
+get  '/lsmbadmin/1.0/:host/:port/db/:dbname/' => sub {
                                                   template 'db_info' =>
                                                             db_info();
                                               };
-post '/lsmbadmin/1.0/:host/:port/db/:dbname' => \&reload_db;
-put  '/lsmbadmin/1.0/:host/:port/db/:dbname' => \&reload_db;
-get  '/lsmbadmin/1.0/:host/:port/db/:dbname/backup' => \&backup_db;
-put  '/lsmbadmin/1.0/:host/:port/db/:dbname/backup' => \&restore_backup;
-post '/lsmbadmin/1.0/:host/:port/db/:dbname/backup' => \&restore_backup;
+post '/lsmbadmin/1.0/:host/:port/db/:dbname/' => \&reload_db;
+put  '/lsmbadmin/1.0/:host/:port/db/:dbname/' => \&reload_db;
+get  '/lsmbadmin/1.0/:host/:port/db/:dbname/backup/*.*' => \&backup_db;
+put  '/lsmbadmin/1.0/:host/:port/db/:dbname/backup/*.*' => \&restore_backup;
+post '/lsmbadmin/1.0/:host/:port/db/:dbname/backup/*.*' => \&restore_backup;
 
 # subs
 
